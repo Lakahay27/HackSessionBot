15b56a8b5da5f9847e06c31a98524b3517238ef6
Lakahay27 <152070199+Lakahay27@users.noreply.github.com>
2024-07-02 18:30:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 28177306
+    API_HASH = "363c71a849c8f6117d66b15d110264a6"
+    TOKEN = "6403163961:AAF1UT_iwlVevFTn7iwAZoe3wP6kjbFXQfI"  
+    START_PIC = "https://telegra.ph/file/ebaba188e58dfaa9ff8a9.jpg" 
+    CHAT = "-1002220303971"    
